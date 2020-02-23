c2607371e7e8942f3b04e59a8c52f9554d91a260
huoyitong <349247397@qq.com>
2020-02-24 00:39:50 +0800

    simple-vuex

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    你的年龄 {{$store.state.age}} <br>
+    我的年龄 {{$store.getters.myAge}}
+    <button @click="syncChange()">点我更改</button>
+    <button @click="asyncChange()">异步更改</button>
+    {{$store.state.a.age}}
+    {{$store.state.b.age}}
+    {{$store.state.b.c.age}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    syncChange() {
+      this.$store.commit('syncChange', 10);
+    },
+    asyncChange() {
+      this.$store.dispatch('asyncChange', 5);
+    }    
+  }
+}
+</script>
+
+<style>
+
+</style>
